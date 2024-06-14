739f38ca24147eee31373d3fa1f171d0698d8e0b
Saskoue <romain.farinacci@hotmail.com>
2024-06-14 21:46:15 +0200

    Core: Database connexion

--- core/Core.php
+++ core/Core.php
@@ -2,18 +2,23 @@
 
     namespace Horizon\Core;
 
+    use Horizon\Core\Database\Database;
     use Horizon\Core\Env\EnvLoader;
     use Horizon\Core\Router\Router;
     class Core {
 
         public function run() {
             require_once './routes/Web.php';
-            EnvLoader::load(__DIR__ . '/../.env');
 
-            $routing = EnvLoader::get('ROUTING_TYPE');
             $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             $method = $_SERVER['REQUEST_METHOD'];
 
-            // Router::dispatch($method, $url);
+            Router::dispatch($method, $url);
+
+            EnvLoader::load('./.env');
+            $DbConnect = EnvLoader::get('DB_ACCESS');
+
+            if($DbConnect === 'true') $Database = new Database;
+            else echo "ok";
         }
     }
---
+++ core/database/Database.php
@@ -0,0 +1,66 @@
+<?php
+
+    namespace Horizon\Core\Database;
+
+    use Horizon\Core\Env\EnvLoader;
+    use PDO;
+    use PDOException;
+    use Exception;
+
+    class Database {
+
+        private static $instance = null;
+        protected $conn;
+
+        public $name;
+        private $password;
+        private $username;
+        private $charset;
+        private $port;
+        private $host;
+
+        public function __construct() {
+            $this->getEnvVars();
+            $this->connect();
+        }
+
+        public static function getInstance(): self {
+            if (self::$instance === null) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+
+        private function getEnvVars() {
+            EnvLoader::load('./.env');
+
+            $this->name = EnvLoader::get('DB_NAME');
+            $this->password = EnvLoader::get('DB_PASSWORD');
+            $this->username = EnvLoader::get('DB_USERNAME');
+            $this->charset = EnvLoader::get('DB_CHARSET');
+            $this->port = EnvLoader::get('DB_PORT');
+            $this->host = EnvLoader::get('DB_HOST');
+        }
+
+        private function connect(): void {
+            try {
+                $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->name};charset={$this->charset}";
+                if ($this->host === 'localhost') {
+                    $dsn .= ';unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock';
+                }
+    
+                $this->conn = new PDO($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "connected";
+            } catch (PDOException $e) {
+                throw new Exception("Database connection error: " . $e->getMessage());
+            }
+        }
+
+        public function getConn(): PDO {
+            return $this->conn;
+        }
+
+        public function __clone() {}
+        public function __wakeup() {}
+    }
