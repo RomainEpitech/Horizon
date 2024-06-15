680fa924f0ecd93a9dcbc56c7d9d44bc91e4d089
Saskoue <romain.farinacci@hotmail.com>
2024-06-15 17:19:53 +0200

    Migration: new migration creation

--- core/commands/CommandHandler.php
+++ core/commands/CommandHandler.php
@@ -2,6 +2,8 @@
 
     namespace Horizon\Core\Commands;
     require './vendor/autoload.php';
+
+    use Horizon\Core\Commands\Tools\HorizonMigration;
     use Horizon\Core\Commands\Tools\HorizonServer;
 
     class CommandHandler {
@@ -11,6 +13,30 @@
                     $runServ = new HorizonServer;
                     $runServ->startServer($params);
                     break;
+                case 'migration:new':
+                    $newMigration = new HorizonMigration();
+                    $newMigration->newMigration();
+                    break;
             }
+        }
+
+        protected function displaySuccessMessage($message) {
+            $greenBackground = "\033[42m";
+            $whiteText = "\033[97m";
+
+            echo "{$greenBackground}{$whiteText}\n";
+            echo str_pad(" ", 80) . "\n";
+            echo str_pad(" $message ", 80, " ", STR_PAD_BOTH) . "\n";
+            echo str_pad(" ", 80) . "\n";
+        }
+
+        protected function displayErrorMessage($message) {
+            $redBackground = "\033[41m";
+            $whiteText = "\033[97m";
+
+            echo "{$redBackground}{$whiteText}\n";
+            echo str_pad(" ", 80) . "\n";
+            echo str_pad(" $message ", 80, " ", STR_PAD_BOTH) . "\n";
+            echo str_pad(" ", 80) . "\n";
         }
     }
---
+++ core/commands/tools/HorizonMigration.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace Horizon\Core\Commands\Tools;
+
+    use Horizon\Core\Commands\CommandHandler;
+    use Horizon\Core\LogHandler;
+    use PDO;
+
+    class HorizonMigration extends CommandHandler {
+
+        public function newMigration() {
+            $timestamp = date('YmdHis');
+            $filePath = __DIR__ . "/../../../migrations/Version{$timestamp}.php";
+            $migrationTemplate = "<?php\n\n    use Core\Commands\Migration\AbstractMigration;\n\n    class Version{$timestamp} extends AbstractMigration {\n\n        public function up() {\n            //\n        }\n\n        public function down() {\n            //\n        }\n    }";
+
+            if (!file_exists(dirname($filePath))) {
+                mkdir(dirname($filePath), 0777, true);
+            }
+
+            file_put_contents($filePath, $migrationTemplate);
+            $log = new LogHandler();
+            // $log->newMigration($timestamp);
+            $this->displaySuccessMessage("Migration created successfully.");
+        }
+    }
