1c54008fffdbff7bc016cb79dd97d2ddbef124e2
Saskoue <romain.farinacci@hotmail.com>
2024-06-18 14:00:17 +0200

    Mystic: Fetch table instance

--- core/mystic/Mystic.php
+++ core/mystic/Mystic.php
@@ -2,6 +2,27 @@
 
     namespace Horizon\Core\Mystic;
 
-    class Mystic {
-        
+    use Horizon\Core\Database\Database;
+    use Exception;
+    use PDO;
+    use PDOException;
+    use ReflectionClass;
+
+    class Mystic extends Database {
+        public static function fetchAll($entityClass) {
+            $db = self::getInstance()->getConn();
+            
+            $reflection = new ReflectionClass($entityClass);
+            $tableName = $reflection->getStaticPropertyValue('tableName');
+    
+            $sql = "SELECT * FROM $tableName";
+            $stmt = $db->prepare($sql);
+            
+            try {
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_CLASS, $entityClass);
+            } catch (PDOException $e) {
+                throw new Exception("SQL execution error: " . $e->getMessage());
+            }
+        }
     }
