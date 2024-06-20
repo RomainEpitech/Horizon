0dcff29496f7f0baa925a64e00a21d72ad3aa374
Saskoue <romain.farinacci@hotmail.com>
2024-06-20 15:17:29 +0200

    Auth: Register user functions

---
+++ core/guards/auth/Auth.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace Horizon\Core\Guards\Auth;
+
+    use Exception;
+    use Horizon\Core\Database\Database;
+    use Horizon\Core\Entities\Users;
+    use Horizon\Core\Mystic\Mystic;
+
+    class Auth {
+        protected $db;
+
+        public function __construct() {
+            $this->db = Database::getInstance()->getConn();
+        }
+
+        public static function hashPassword($password) {
+            return password_hash($password, PASSWORD_BCRYPT);
+        }
+
+        public static function registerUser($params) {
+            if (!isset($params['password']) || !isset($params['confirm_password'])) {
+                throw new Exception("Password and Confirm Password fields are required.");
+            }
+            
+            if ($params['password'] !== $params['confirm_password']) {
+                throw new Exception("Passwords do not match.");
+            }
+
+            $params['password'] = self::hashPassword($params['password']);
+            unset($params['confirm_password']);
+
+            $keys = array_keys($params);
+
+            try {
+                return Mystic::insert(Users::class, $params);
+            } catch (Exception $e) {
+                throw new Exception("Failed to register user: " . $e->getMessage());
+            }
+        }
+    }
--- src/controllers/RegisterController.php
+++ src/controllers/RegisterController.php
@@ -4,22 +4,20 @@
 
     use Horizon\Core\CoreController;
     use Horizon\Core\Entities\Users;
+    use Horizon\Core\Guards\Auth\Auth;
     use Horizon\Core\Guards\Http\Request;
     use Horizon\Core\Mystic\Mystic;
 
     class RegisterController extends CoreController {
         public function index(Request $request) {
             if ($request->method('POST')) {
-                $email = $request->validateEmail('email');
-                $password = $request->sanitizeString('password');
-                echo "password: " . $password . "email : " . $email;
-                $newUser = new Mystic();
-                $newUser->insert(Users::class, [
-                    "email" => $email,
-                    "password" => $password
-                ]);
+                $params = Request::getFields();
+
+                // $newUser = Mystic::insert(Users::class, $params);
+                $newUser = Auth::registerUser($params);
 
                 if ($newUser) {
+                    print_r($params);
                     echo "new user";
                 } else {
                     die("failed");
