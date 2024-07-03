c713fb31fb47ca0c1a063cfd4e08e6c7d4c163b7
Saskoue <romain.farinacci@hotmail.com>
2024-07-03 15:32:03 +0200

    Fix: ApiRouter allow middleware

--- core/router/ApiRouter.php
+++ core/router/ApiRouter.php
@@ -3,25 +3,23 @@
     namespace Horizon\Core\Router;
 
     class ApiRouter extends Router {
-        public static function connect($method, $url, $controllerAction) {
-            parent::connect($method, '/api' . $url, $controllerAction);
+        public static function connect($method, $url, $controllerAction, $middleware = []) {
+            parent::connect($method, '/api' . $url, $controllerAction, $middleware);
         }
 
-        public static function get($url, $controllerAction) {
-            self::connect('GET', $url, $controllerAction);
+        public static function get($url, $controllerAction, $middleware = []) {
+            self::connect('GET', $url, $controllerAction, $middleware);
         }
 
-        public static function post($url, $controllerAction) {
-            self::connect('POST', $url, $controllerAction);
+        public static function post($url, $controllerAction, $middleware = []) {
+            self::connect('POST', $url, $controllerAction, $middleware);
         }
 
-        public static function put($url, $controllerAction)
-        {
-            self::connect('PUT', $url, $controllerAction);
+        public static function put($url, $controllerAction, $middleware = []) {
+            self::connect('PUT', $url, $controllerAction, $middleware);
         }
 
-        public static function delete($url, $controllerAction)
-        {
-            self::connect('DELETE', $url, $controllerAction);
+        public static function delete($url, $controllerAction, $middleware = []) {
+            self::connect('DELETE', $url, $controllerAction, $middleware);
         }
     }
